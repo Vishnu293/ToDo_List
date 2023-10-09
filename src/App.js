2f97195e4763ef69df6224d5f20054c90d6ea610
Vishnu293 <vishnuyadav2932002@gmail.com>
2023-10-09 23:29:12 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }  from 'react'
 
-function App() {
+const App = () => {
+  const [info, showMore] = useState("loremofnfffaf ajfand WK");
+
+  const eventHandler = () => {
+    return (
+      <div>
+      <p>xloreee ee eaeref fefaeaee rereafaegbaegaebagea egegag</p>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1>My Counter App</h1>
+        {info}<br></br>
+        <button onClick={eventHandler}>Show More</button>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
